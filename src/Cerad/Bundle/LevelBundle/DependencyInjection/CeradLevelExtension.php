3696f9522da6fa573f01e7b6060c14026cefb4fd
ahundiak <ahundiak@spike.mshome.net>
2013-09-21 07:03:46 -0500

    Level Service

--- src/Cerad/Bundle/LevelBundle/DependencyInjection/CeradLevelExtension.php
+++ src/Cerad/Bundle/LevelBundle/DependencyInjection/CeradLevelExtension.php
@@ -24,5 +24,7 @@ class CeradLevelExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        
+        $container->setParameter('cerad_level_resources_dir',__DIR__.'/../Resources');
     }
 }
