d0fc990c6d296a0ef2275a0e342e499ef8b31352
ahundiak <ahundiak@spike.mshome.net>
2013-09-23 10:40:31 -0500

    S1Games Import

--- src/Cerad/Bundle/GameBundle/Entity/GameField.php
+++ src/Cerad/Bundle/GameBundle/Entity/GameField.php
@@ -5,18 +5,21 @@ class GameField extends AbstractEntity
 {
     protected $id;
     
+    protected $sort;
     protected $name;
     protected $venue;
     protected $status = 'Active';
     protected $projectId;
     
     public function getId()        { return $this->id;        }
+    public function getSort()      { return $this->sort;      }
     public function getName()      { return $this->name;      }
     public function getVenue()     { return $this->venue;     }
     public function getStatus()    { return $this->status;    }
     public function getProjectId() { return $this->projectId; }
     
     public function setId       ($value) { $this->onPropertySet('id',       $value); }
+    public function setSort     ($value) { $this->onPropertySet('sort',     $value); }
     public function setName     ($value) { $this->onPropertySet('name',     $value); }
     public function setVenue    ($value) { $this->onPropertySet('venue',    $value); }
     public function setStatus   ($value) { $this->onPropertySet('status',   $value); }   
