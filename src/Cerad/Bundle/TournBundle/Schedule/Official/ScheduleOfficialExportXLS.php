6f3c141041976138ebab57a1f146a45ab941f069
ahundiak <ahundiak@spike.mshome.net>
2013-10-29 10:18:00 -0500

    Added officials to game official spreadsheet.

--- src/Cerad/Bundle/TournBundle/Schedule/Official/ScheduleOfficialExportXLS.php
+++ src/Cerad/Bundle/TournBundle/Schedule/Official/ScheduleOfficialExportXLS.php
@@ -19,6 +19,8 @@ class ScheduleOfficialExportXLS
         'Home Team' => 26, 'Away Team' => 26,
         
         'Referee' => 26, 'AR1' => 26, 'AR2' => 26,
+        
+        'Name' => 26, 'Pos' => 6, 'YC' => 3, 'RC' => 3,
     );
     protected $center = array
     (
@@ -54,6 +56,9 @@ class ScheduleOfficialExportXLS
         }
         return $row;
     }
+    /* ========================================================
+     * Generates the games listing
+     */
     public function generateGames($ws,$games)
     {
         // Only the keys are currently being used
@@ -119,6 +124,9 @@ class ScheduleOfficialExportXLS
         }
         return;
     }
+    /* =======================================================================
+     * Main entry point
+     */
     public function generate($games)
     {
         // Spreadsheet
@@ -134,6 +142,9 @@ class ScheduleOfficialExportXLS
         
         $this->generateGames($ws,$games);
         
+        $ws1 = $ss->createSheet(1);
+        $this->processOfficials($ws1,$games);
+        
         // Output
         $ss->setActiveSheetIndex(0);
         $objWriter = $this->excel->newWriter($ss); // \PHPExcel_IOFactory::createWriter($ss, 'Excel5');
@@ -141,6 +152,115 @@ class ScheduleOfficialExportXLS
         ob_start();
         $objWriter->save('php://output'); // Instead of file name
         return ob_get_clean();
+    }
+    /* ===========================================================
+     * Add a sheet listing current assignments for each official
+     * Should probably get moved to it's own processor
+     */
+    protected function processOfficials($ws,$games)
+    {
+        // Make a sorted array of officials from games
+        $officials = array();
+        foreach($games as $game)
+        {
+            foreach($game->getOfficials() as $official)
+            {
+                $name = $official->getPersonNameFull();
+                if ($name)
+                {
+                    $officials[$name][] = $official;
+                }
+            }
+        }
+        ksort($officials);
+        
+        // Genetare
+        $this->generateOfficials($ws,$officials);
+    }
+    /* ========================================================
+     * Generates the officials listing
+     */
+    public function generateOfficials($ws,$officials)
+    {
+        // Only the keys are currently being used
+        $map = array(
+            'Name'     => 'name',
+            'Pos'      => 'pos',
+            'YC'       => 'yc',
+            'RC'       => 'rc',
+            'Game'     => 'game',
+            'Date'     => 'date',
+            'DOW'      => 'dow',
+            'Time'     => 'time',
+            'Venue'    => 'venue',
+            'Field'    => 'field',
+            'Type'     => 'type',
+            'Pool'     => 'pool',
+            
+            'Home Team' => 'homeTeam',
+            'Away Team' => 'awayTeam',
+        );
+        $ws->setTitle('Officials');
+        
+        $row = $this->setHeaders($ws,$map);
+        
+        foreach($officials as $officialSlots)
+        {   
+            $row++;
+            
+            foreach($officialSlots as $official) {
+                
+            
+            $row++;
+            $col = 0;
+            
+            // Official
+            $name = $official->getPersonNameFull();
+            $pos  = $official->getRole();
+            $game = $official->getGame();
+            
+            $ws->setCellValueByColumnAndRow($col++,$row,$name);
+            $ws->setCellValueByColumnAndRow($col++,$row,$pos);
+           
+            // Cards
+            $yc = $rc = 0;
+            
+            $homeTeamReport = $game->getHomeTeam()->getReport();
+            $awayTeamReport = $game->getAwayTeam()->getReport();
+            
+            if ($homeTeamReport->getPlayerWarnings()) $yc += $homeTeamReport->getPlayerWarnings();
+            if ($awayTeamReport->getPlayerWarnings()) $yc += $awayTeamReport->getPlayerWarnings();
+            
+            if ($homeTeamReport->getPlayerEjections()) $rc += $homeTeamReport->getPlayerEjections();
+            if ($awayTeamReport->getPlayerEjections()) $rc += $awayTeamReport->getPlayerEjections();
+            
+            if ($yc) $ws->setCellValueByColumnAndRow($col++,$row,$yc);
+            else     $col++;
+            if ($rc) $ws->setCellValueByColumnAndRow($col++,$row,$rc);
+            else     $col++;
+            
+            // Date/Time
+            $dt   = $game->getDtBeg();
+            $dow  = $dt->format('D');
+            $date = $dt->format('M d y');
+            $time = '_' . $dt->format('H:i A'); //('g:i A');
+            
+            // Break this out later
+            // Need to follow game sheet format to support easy copy/paste
+            $ws->setCellValueByColumnAndRow($col++,$row,$game->getNum());
+            $ws->setCellValueByColumnAndRow($col++,$row,$date);
+            $ws->setCellValueByColumnAndRow($col++,$row,$dow);
+            $ws->setCellValueByColumnAndRow($col++,$row,$time);
+            $ws->setCellValueByColumnAndRow($col++,$row,$game->getField()->getVenue());
+            $ws->setCellValueByColumnAndRow($col++,$row,$game->getField()->getName ());
+            $ws->setCellValueByColumnAndRow($col++,$row,$game->getGroup());
+            $ws->setCellValueByColumnAndRow($col++,$row,$game->getLevelId());
+            
+            $ws->setCellValueByColumnAndRow($col++,$row,$game->getHomeTeam()->getName());
+            $ws->setCellValueByColumnAndRow($col++,$row,$game->getAwayTeam()->getName());
+            
+        }}
+        return;
     }
 }
 ?>
