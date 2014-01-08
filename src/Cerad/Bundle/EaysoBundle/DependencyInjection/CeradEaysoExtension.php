3a3366a5e4dfc4c6f84d0940fea47e5e0f5c4b74
ahundiak <ahundiak@spike.mshome.net>
2014-01-08 16:45:58 -0600

    Fed design

--- src/Cerad/Bundle/EaysoBundle/DependencyInjection/CeradEaysoExtension.php
+++ src/Cerad/Bundle/EaysoBundle/DependencyInjection/CeradEaysoExtension.php
@@ -24,5 +24,7 @@ class CeradEaysoExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        
+        $container->setParameter('cerad_eayso_resources_dir',__DIR__.'/../Resources');
     }
 }
---
+++ src/Cerad/Bundle/EaysoBundle/InMemory/CertRepository.php
@@ -0,0 +1,53 @@
+<?php
+namespace Cerad\Bundle\EaysoBundle\InMemory;
+
+use Symfony\Component\Yaml\Yaml;
+
+class CertRepository 
+{
+    protected $certs = array();
+    protected $certDescs = null;
+    
+    public function __construct($file)
+    {
+        $yaml = Yaml::parse(file_get_contents($file));
+        $this->certs = $yaml['certs'];
+    }
+    public function findByCertDesc($certDesc)
+    {
+        // Transform is necessary
+        if (!$this->certDescs)
+        {
+            $certDescs = array();
+            foreach($this->certs as $role => $badges)
+            {
+                foreach($badges as $badge => $descs)
+                {
+                    foreach($descs as $desc)
+                    {
+                        $certDescs[$desc][$role] = $badge;
+                    }
+                }
+            }
+            $this->certDescs = $certDescs;
+        }
+        if (isset($this->certDescs[$certDesc])) return $this->certDescs[$certDesc];
+        
+        echo sprintf("*** No cert record for '%s'\n",$certDesc);
+        return array();
+    }
+    // Referee Intermediate National
+    public function compareBadges($role,$badge1,$badge2)
+    {
+        if ($badge1 == $badge2) return 0;
+        
+        $badges = $this->certs[$role];
+        foreach($badges as $badge => $descs)
+        {
+            if ($badge == $badge1) return -1;
+            if ($badge == $badge2) return  1;
+        }
+    }
+}
+
+?>
