9c021bc299e54b5034c8704cb42f04df37bcc6f1
ahundiak <ahundiak@spike.mshome.net>
2014-05-14 11:06:18 -0500

    Team Schedule Spreadsheet

--- src/Cerad/Bundle/GameBundle/Action/Project/Schedule/Team/Show/ScheduleTeamShowController.php
+++ src/Cerad/Bundle/GameBundle/Action/Project/Schedule/Team/Show/ScheduleTeamShowController.php
@@ -4,11 +4,12 @@ namespace Cerad\Bundle\GameBundle\Action\Project\Schedule\Team\Show;
 use Cerad\Bundle\CoreBundle\Action\ActionController;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class ScheduleTeamShowController extends ActionController
 {
-    public function action(Request $request, ScheduleTeamShowModel $model, $form)
+    public function action(Request $request, ScheduleTeamShowModel $model, $form, $_format)
     {
         $form->handleRequest($request);
         if ($form->isValid()) 
@@ -19,6 +20,32 @@ class ScheduleTeamShowController extends ActionController
             return new RedirectResponse($formAction);  // To form
         }
         $games = $model->loadGames();
+        
+        // Spreadsheet
+        if ($_format == 'xls')
+        {
+            $export   = new ScheduleTeamExportXLS();
+            $response = new Response($export->generate($games));
+        
+            $outFileName = 'TeamSchedule' . date('Ymd-Hi') . '.xlsx';
+        
+            $response->headers->set('Content-Type',       'application/vnd.ms-excel');
+            $response->headers->set('Content-Disposition', sprintf('attachment; filename="%s"',$outFileName));
+            return $response;
+        }
+        // csv processing
+        if ($_format == 'csv')
+        {
+            // Should be a service but that means injecting it somehow
+            $export   = new ScheduleTeamExportCSV();
+            $response = new Response($export->generate($games));
+        
+            $outFileName = 'TeamSchedule' . date('Ymd-Hi') . '.csv';
+        
+            $response->headers->set('Content-Type',       'text/csv;');
+            $response->headers->set('Content-Disposition', sprintf('attachment; filename="%s"',$outFileName));
+            return $response;
+        }
         
         // And render
         $tplData = array();
