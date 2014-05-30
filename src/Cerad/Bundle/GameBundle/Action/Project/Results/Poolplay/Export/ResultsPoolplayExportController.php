1a369bef16022bd993cf19180b9ecca5fcf5615b
ahundiak <ahundiak@spike.mshome.net>
2014-05-30 15:37:30 -0500

    Export Poolplay Results

---
+++ src/Cerad/Bundle/GameBundle/Action/Project/Results/Poolplay/Export/ResultsPoolplayExportController.php
@@ -0,0 +1,15 @@
+<?php
+namespace Cerad\Bundle\GameBundle\Action\Project\Results\Poolplay\Export;
+
+use Cerad\Bundle\CoreBundle\Action\ActionController;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class ResultsPoolplayExportController extends ActionController
+{
+    public function action(Request $request)
+    {
+        // Might want to do some role checking here
+        return;
+    }
+}
