349b0f5671b17488097de6caa1f67ec1c6cdf37b
ahundiak <ahundiak@spike.mshome.net>
2014-06-18 06:25:44 -0500

    Updated PersonTeam

--- src/Cerad/Bundle/PersonBundle/Model/PersonTeam.php
+++ src/Cerad/Bundle/PersonBundle/Model/PersonTeam.php
@@ -24,30 +24,53 @@ class PersonTeam
     
     protected $person;
     
-    protected $num;
-    protected $name;
+    protected $teamKey;
+    protected $teamName;
+    protected $teamDesc;
     
     protected $levelKey;
     protected $projectKey;
     
     protected $status = 'Active';
     
-    public function getNum     ()   { return $this->num;        }
-    public function getName    ()   { return $this->name;       }
+    public function getTeamKey ()   { return $this->teamKey;    }
+    public function getTeamName()   { return $this->teamName;   }
+    public function getTeamDesc()   { return $this->teamDesc;   }
+    
     public function getRole    ()   { return $this->role;       }
     public function getPerson  ()   { return $this->person;     }
     public function getStatus  ()   { return $this->status;     }
     public function getLevelKey()   { return $this->levelKey;   }
     public function getProjectKey() { return $this->projectKey; }
     
-    public function setNum     ($v)   { $this->num        = $v; return $this; }
-    public function setName    ($v)   { $this->name       = $v; return $this; }
+    // Don't really need team setters
+    public function setTeamKey ($v) { $this->teamKey  = $v; return $this; }
+    public function setTeamName($v) { $this->teamName = $v; return $this; }
+    public function setTeamDesc($v) { $this->teamDesc = $v; return $this; }
+    
     public function setRole    ($v)   { $this->role       = $v; return $this; }
     public function setPerson  ($v)   { $this->person     = $v; return $this; }
     public function setStatus  ($v)   { $this->status     = $v; return $this; }
     public function setLevelKey($v)   { $this->levelKey   = $v; return $this; }
     public function setProjectKey($v) { $this->projectKey = $v; return $this; }
     
-    
+    public function setTeam($team)
+    {
+        if (!$team)
+        {
+            // This reall should not happen, clearing a team should delete the whole entry
+            $this->teamKey  = null;
+            $this->teamName = null;
+            $this->teamDesc = null;
+            $this->levelKey = null;
+            $this->projectKey = null; 
+            return;
+        }
+        $this->teamKey    = $team->getKey();
+        $this->teamName   = $team->getName();
+        $this->teamDesc   = $team->getDesc();
+        $this->levelKey   = $team->getLevelKey();
+        $this->projectKey = $team->getProjectKey();
+    }
 }
 ?>
